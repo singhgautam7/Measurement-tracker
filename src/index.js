35dd4ad01eb00f9666340dd8d505d70ff0b5dfc3
Gautam Rajeev Singh <31505446+singhgautam7@users.noreply.github.com>
2023-10-14 04:06:10 +0530

    undo last commit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './src/App';
-import store from './src/store/store'
+import App from './App';
+import store from './store/store'
 import { Provider } from 'react-redux'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
